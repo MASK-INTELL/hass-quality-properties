926f76b4b5cd71f0ab3caeff5fdbab352fc6883f
MASK INTELLIGENCE <maskintelligence@gmail.com>
2026-06-05 07:01:34 +0300

    Remove ScrollToTop - conflicts with Next.js built-in scroll restoration

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,10 +1,8 @@
 import PublicLayout from '@/components/PublicLayout';
-import ScrollToTop from '@/components/ScrollToTop';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <PublicLayout>
-      <ScrollToTop />
       {children}
     </PublicLayout>
   );
